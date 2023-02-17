2dec4de92019d38d3c54350b3ecda9bbcc8a0c5c
Guilherme Rocha <guilherme.mesquita.rocha@gmail.com>
2023-02-17 16:52:06 -0300

    Finalizando proejto

--- src/app/pages/orbit/components/earth/earth.component.ts
+++ src/app/pages/orbit/components/earth/earth.component.ts
@@ -9,8 +9,40 @@ import * as moment from 'moment';
 export class EarthComponent implements OnInit {
   constructor() {}
   startDate: moment.Moment = moment('2022-07-12');
+  public days: number = 0;
+  public hours: number = 0;
+  public minutes: number = 0;
+  public seconds: number = 0;
+  public anniversaryDay: number = 12; // Dia do aniversário de namoro
+  public diff: number = 0;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let today = new Date();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    let nextAnniversaryMonth = month;
+    if (month >= 11) {
+      nextAnniversaryMonth = 0;
+      year += 1;
+    } else {
+      nextAnniversaryMonth += 1;
+    }
+    let nextAnniversaryDate = new Date(
+      year,
+      nextAnniversaryMonth,
+      this.anniversaryDay
+    );
+    this.diff = nextAnniversaryDate.getTime() - today.getTime();
+    setInterval(() => {
+      this.diff = nextAnniversaryDate.getTime() - new Date().getTime();
+      this.days = Math.floor(this.diff / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor(
+        (this.diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      this.minutes = Math.floor((this.diff % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((this.diff % (1000 * 60)) / 1000);
+    }, 1000);
+  }
 
   @HostListener('window:scroll', ['$event']) onScrollAnimation(event: any) {
     let stars: ElementRef | any = document.getElementById('stars');
